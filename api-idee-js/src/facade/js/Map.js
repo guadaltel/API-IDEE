5996e0b81aa581b0c6cbae4039a794441bcbb527
Guadaltel <gdtelcnig_develop@guadaltel.com>
2025-10-20 10:36:31 +0200

    Se soluciona problemas compilacion

--- api-idee-js/src/facade/js/Map.js
+++ api-idee-js/src/facade/js/Map.js
@@ -4750,7 +4750,7 @@ class Map extends Base {
     this.removeLayers(layers);
     return this;
   }
-    
+
   /**
    * Este método permite activar o desactivar la interacción de panneo.
    * El valor por defecto es true.
