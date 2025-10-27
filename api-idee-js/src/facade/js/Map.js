f0ec2a8e16f0083f1b79e2069d3519e35eb65ffa
Guadaltel <gdtelcnig_develop@guadaltel.com>
2025-10-27 14:16:53 +0100

    501: se muestra warning al hacer uso de enabledrag

--- api-idee-js/src/facade/js/Map.js
+++ api-idee-js/src/facade/js/Map.js
@@ -4746,7 +4746,7 @@ class Map extends Base {
   getImageMap(type = 'image/jpeg', canva = undefined, isPromise = false) {
     return getImageMap(this, type, canva, isPromise);
   }
-  
+
   /**
    * Este método controla si la interacción de zoom con la rueda del ratón está activa o no.
    * El valor por defecto es true
@@ -4811,6 +4811,8 @@ class Map extends Base {
    * @deprecated
    */
   enableDrag(active) {
+    // eslint-disable-next-line no-console
+    console.warn(getValue('exception').enableDrag_deprecated);
     this.enablePan(active);
   }
 }
