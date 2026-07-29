3761fba11e0d41e65deab6f15e9f31b0f6abe09b
gdtelcnig <gdtelcnig_develop@guadaltel.com>
2026-07-29 08:03:22 +0200

    actualizo test

--- api-idee-js/src/plugins/rastermanagement/test/test.js
+++ api-idee-js/src/plugins/rastermanagement/test/test.js
@@ -5,62 +5,57 @@ IDEE.language.setLang('es');
 
 const map = IDEE.map({
   container: 'mapjs',
-  bbox: [3226511.5398818217, 1735204.4920150614, 4207995.393869615, 2056231.5025902353],
-  // bbox: [-757523.4071233872, 4442339.45919535, -731974.5591508334, 4476483.050592874]
-  // bbox: [2713214.478758277, 5389452.879113358, 2719331.5689785494, 5391744.30804452]
-  // bbox: [-482568.7151175922, 4903327.95112306, -328387.66404849157, 4945968.648059358]
-});
+  // bbox: [3226511.5398818217, 1735204.4920150614, 4207995.393869615, 2056231.5025902353],
+ });
 window.map = map;
 
-
 map.addLayers(new IDEE.layer.GeoTIFF({
-  url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/36/Q/WD/2020/7/S2A_36QWD_20200701_0_L2A/TCI.tif',
+  url: 'https://e84-earth-search-sentinel-data.s3.us-west-2.amazonaws.com/sentinel-2-pre-c1-l2a/21/N/YC/2022/12/S2B_T21NYC_20221205T140704_L2A/B04.tif',
   name: 'Sentinel TCI',
   legend: 'Sentinel-2 color verdadero',
 }, {
   normalize: true,
   nodata: 0,
   style: new IDEE.style.Raster({
-    saturation: 1,
+    bands: [1,0,0],
+    // nodata: 0,
+    gamma: 2
   }),
 }));
 
 
-// NDWI: 4 Y 2
-// NDVI: 4 Y 1
 // map.addLayers(new IDEE.layer.GeoTIFF({
-//   url: './files/IMG_PH1A_PHR_MS___3_20180718T112527_20180718T112530_TOU_1234_e991_R1C1_COG.tif',
-//   name: 'NDVI_Extremadura Severidad',
-//   legend: 'NDVI_Extremadura Severidad',
+//   url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/36/Q/WD/2020/7/S2A_36QWD_20200701_0_L2A/TCI.tif',
+//   name: 'Sentinel TCI',
+//   legend: 'Sentinel-2 color verdadero',
 // }, {
 //   normalize: true,
-//   convertToRGB: false,
+//   nodata: 0,
 //   style: new IDEE.style.Raster({
-//     "bands": [
-//     3,
-//     1,
-//     4
-//     ],
-//     "nodata": 0,
-//     "gamma": 2,
-//     "saturation": 0,
-//     "exposure": 0,
-//     "contrast": 0,
-//     "brightness": 0
-//     }),
+//     saturation: 1,
+//     nodata: 0,
+//   }),
+// }));
+
+// NIR: 4
+// SWIR: 6
+// AZUL: 1
+// ROJO: 3
+// VERDE: 2
+
+// Para NDVI min: 0,001632 max: 0,46432
+// Para NDWI min: -0.45 max: -0.011
+// Para NDWI min: -0.09 max: 0.33
+// map.addLayers(new IDEE.layer.GeoTIFF({
+//   url: './tif/original_COG.tiff',
+//   name: 'falsocolor',
+//   legend: 'falsocolor',
+// }, {
+//   normalize: true,
 //   nodata: 0,
 // }));
 
-// // NIR SWIR1: 4 Y 5
-// map.addLayers(new IDEE.layer.GeoTIFF({
-//   url: './files/AnnualCrop_RGB_NIR_SWIR1.tif',
-//   name: 'Sentinel RGB+NIR+SWIR',
-//   legend: 'Sentinel RGB+NIR+SWIR',
-// }, {
-//   normalize: true,
-//   convertToRGB: false,
-//   nodata: 0,
-// }));
+
 
 
 const mp = new RasterManagement({
